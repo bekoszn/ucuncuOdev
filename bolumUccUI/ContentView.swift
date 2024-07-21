9548601fb4071cdf8865b5b145fd9c584662c6c5
bekoszn <berkeozguder@gmail.com>
2024-07-21 22:04:23 +0300

    Tasarım Çalışması
    SwiftUI kullanarak başarılı bir şekilde bir ana sayfa tasarımı çalışması yaptım

--- bolumUccUI/ContentView.swift
+++ bolumUccUI/ContentView.swift
@@ -10,10 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack {
+                    
+                    Text("Plant").bold().font(.system(size: 40))
+                    Text("a tree").bold().font(.system(size: 40))
+                    Text("for life").bold().font(.system(size: 40))
+                }
+                Spacer()
+                Spacer()
+                Image("pngegg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160, height: 160)
+                    
+            }
+
+            Image("plant")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 500, height: 500)
+            
+            Text("All over country delivery within 6-8 days.")
+                .font(.system(size:15))
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+            Spacer()
+            Spacer()
+            
+            Button("Add to cart") {}
+                .buttonStyle(.borderedProminent)
+                .tint(Color.green)
         }
         .padding()
     }
