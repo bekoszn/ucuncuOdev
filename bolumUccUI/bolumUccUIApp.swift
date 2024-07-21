d5326a595b860037876e0af08145666b3e073761
bekoszn <berkeozguder@gmail.com>
2024-07-21 21:09:26 +0300

    Initial Commit

---
+++ bolumUccUI/bolumUccUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  bolumUccUIApp.swift
+//  bolumUccUI
+//
+//  Created by Berke Özgüder on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct bolumUccUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
